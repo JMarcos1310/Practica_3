a69f4b23bb1b9e3f6ad1f3a0e23c6b4751c42a03
JMarcos1310 <136421801+JMarcos1310@users.noreply.github.com>
2023-12-07 18:55:41 -0600

    Practica

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+</head>
+<body>
+    <section class="seccion-con-fondo">
+        <div class="capa-oscura"></div>
+        <div class="contenido">
+        <form action="" method="get">
+            <h2>Sección con Imagen de Fondo</h2>
+            <p>Este es un ejemplo de una sección con una imagen de fondo y una capa oscura.</p>
+        </form>      
+        </div>
+    </section>
+</body>
+</html>
